25dcfb46958e48fee8e2db170c0f92b513524885
Jabroni303 <60592125+Jabroni303@users.noreply.github.com>
2022-09-09 12:12:37 -0600

    Update main.java

--- Part1/main.java
+++ Part1/main.java
@@ -1,8 +1,8 @@
 
 public class main{
     public static void  main(String[] args){
-    Processor processor = new Processor();
-    Processor.execute();
+    Processor processor = new Processor(); // instantiate new preocessor
+    Processor.execute(); //calls all methods in processor
 
 
 }
